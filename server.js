572c9f404c6110e2250ea924a98cd9893f525a57
Jessica <jessicamenius@gmail.com>
2020-08-22 13:54:06 -0500

    added material ui

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("./client/"));
 
-const apiRoutes = require("./routes/api_routes");
-app.use(apiRoutes);
-
-const clientRotes = require("./routes/client_routes");
+const apiRoutes = require("./routes/api-routes");
 app.use(apiRoutes);
 
 app.listen(PORT, () => console.log(`Listening at: http://localhost:${PORT}`));
