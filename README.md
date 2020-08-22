1ceb142fb414fdc9966e5e09b032a018dfae25bb
Jessica <jessicamenius@gmail.com>
2020-08-22 14:31:10 -0500

    make it better

---
+++ README.md
@@ -0,0 +1,9 @@
+# Project 3 - Health Tracker
+
+```
+As someone interested in nutrition and health
+I want to be able to add meals and snacks todaily tracker
+So that I can count calories and lose/gain weight
+```
+
+## Instructions
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const session = require("express-session");
+const passport = require("passport");
+const db = require("./models");
+const PORT = process.env.PORT || 3000;
+require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
