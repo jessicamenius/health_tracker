3baaa91f59422725c48bf1c4b059c267ccba11de
Philip Kouchner <philipk179@gmail.com>
2020-08-25 15:55:29 -0700

    add inputs

--- health-tracker/src/App.js
+++ health-tracker/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import UserForm from "./components/UserForm";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <UserForm />
     </div>
   );
 }
