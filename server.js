64430c7fe792a3197c12e5cbfc8abe36e577e0ac
Mat Wilmot <matwilmot90@gmail.com>
2020-08-24 22:11:31 -0700

    fixed server.js file

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 const express = require("express");
 const app = express();
-const session = require("express-session");
-const passport = require("passport");
-const db = require("./models");
-require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const path = require("path");
 const db = require("./models");
