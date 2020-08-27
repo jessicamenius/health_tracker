e94abf48f72ca17f388ff863dea2f9b81e959aad
Philip Kouchner <philipk179@gmail.com>
2020-08-26 17:17:17 -0700

    building the userform

--- health-tracker/src/pages/Login.js
+++ health-tracker/src/pages/Login.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import FormSearch from '../components/FormSearch'
+
 
 const Login = () => {
     return (
         <div>
             <h1>Hello Welcome to Login Page</h1>
+            <FormSearch />
         </div>
     )
 }
