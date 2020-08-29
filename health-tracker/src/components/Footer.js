4e8ba0209bcf699d55276811b696aac8a0a53599
Mat Wilmot <matwilmot90@gmail.com>
2020-08-29 13:17:24 -0700

    fix conflict

--- health-tracker/src/components/Footer.js
+++ health-tracker/src/components/Footer.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
-
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 const Footer = () => {
+<<<<<<< HEAD
+
+
 
     function Copyright() {
         return (
             <Typography variant="body2" color="textSecondary" align="center">
                 {"Copyright © "}
-                <Link color="inherit" href="https://material-ui.com/">
+                <Link color="primary">
                     Developers Jessica, Philip, Mat, Steven
             </Link>{" "}
                 {new Date().getFullYear()}
@@ -22,8 +23,7 @@ const Footer = () => {
     const useStyles = makeStyles((theme) => ({
         root: {
             display: 'flex',
-            flexDirection: 'column',
-            minHeight: "100vh",
+            flexDirection: 'column'
         },
         main: {
             marginTop: theme.spacing(8),
@@ -40,7 +40,7 @@ const Footer = () => {
         <div className={classes.root}>
             <footer footer="true" className={classes.footer}>
                 <Container maxWidth="sm">
-                    <Typography variant="body1">
+                    <Typography variant="body1" style={{ textAlign: "center" }}>
                         My Exercise Rest Nutrition Tracker (MERN-Tracker)
                     </Typography>
                     <Copyright />
@@ -50,4 +50,47 @@ const Footer = () => {
     );
 }
 
-export default Footer
+
+        {new Date().getFullYear()}
+=======
+  function Copyright() {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {"Copyright © "} {new Date().getFullYear()}
+>>>>>>> 26c5c8f59514327147c22816480eeb20df296ae4
+        {"."}
+      </Typography>
+    );
+  }
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: "flex",
+      flexDirection: "column",
+    },
+    main: {
+      marginTop: theme.spacing(8),
+      marginBottom: theme.spacing(2),
+    },
+    footer: {
+      padding: theme.spacing(3, 2),
+      marginTop: "auto",
+    },
+  }));
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <footer footer="true" className={classes.footer}>
+        <Container maxWidth="sm">
+          <Typography variant="body1" style={{ textAlign: "center" }}>
+            My Exercise Rest Nutrition Tracker (MERN-Tracker)
+          </Typography>
+          <Typography variant="body2" color="textSecondary" align="center">
+            Developers Jessica, Philip, Mat, Steven
+          </Typography>
+          <Copyright />
+        </Container>
+      </footer>
+    </div>
+  );
+};
+export default Footer;
--- health-tracker/src/components/FormGoals.js
+++ health-tracker/src/components/FormGoals.js
@@ -1,73 +1,65 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-
+import Button from '@material-ui/core/Button';
 const FormGoals = () => {
+  let objUserStats = {};
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      "& > *": {
+        margin: theme.spacing(1),
+        width: "25ch",
+      },
+    },
+    container: {
+      display: "flex",
+      justifyContent: "center",
+      flexDirection: "row",
+      margin: "100px",
+    },
+    divOne: {
+      height: "50vh",
+      width: "50%",
+    },
+    form: {
+      display: "flex",
+      justifyContent: "center",
+      alignContent: "center",
+      position: "static",
+      flexDirection: "column",
+      width: "80%",
+      marginLeft: "50px",
+      marginTop: "50px",
+    },
+    button: {
+      display: "flex",
+      justifyContent: "center",
+      alignContent: "center",
+      marginTop: "50px",
+    },
+  }));
 
-    let objUserStats = {};
-    const useStyles = makeStyles((theme) => ({
-        root: {
-            '& > *': {
-                margin: theme.spacing(1),
-                width: '25ch',
-            },
-        },
-        container: {
-            display: "flex",
-            justifyContent: "center",
-            flexDirection: "row",
-            margin: "100px"
-        },
-        divOne: {
-            height: "50vh",
-            width: "50%"
-        },
-        form: {
-            display: "flex",
-            justifyContent: "center",
-            alignContent: "center",
-            position: "static",
-            flexDirection: "column",
-            width: "80%",
-            marginLeft: "50px",
-            marginTop: "50px"
-        },
-        button: {
-            display: "flex",
-            justifyContent: "center",
-            alignContent: "center",
-            marginTop: "50px"
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [age, setAge] = useState(0);
+  const [gender, setGender] = useState(0);
+  const [answer, setAnswer] = useState(false);
+  const [resultBMI, setResult] = useState(0);
+  const [resultBMR, setResultBMR] = useState(0);
 
-        }
-
-    }));
-
-    const [height, setHeight] = useState(0);
-    const [weight, setWeight] = useState(0);
-    const [age, setAge] = useState(0);
-    const [gender, setGender] = useState(0);
-    const [answer, setAnswer] = useState(false);
-    const [resultBMI, setResult] = useState(0);
-    const [resultBMR, setResultBMR] = useState(0);
-
-    const submitBMI = (e) => {
-        e.preventDefault();
-        objUserStats = {
-
-            height: height,
-            weight: weight,
-            age: age,
-            gender: gender
-        }
-        // function that calculate the BMI of the use and keep the data
-        if (answer) {
-
-            setAnswer(false);
-        } else {
-            setAnswer(true)
-        }
-        let temp = (703 * weight) / Math.pow(height, 2);
-        setResult(temp);
+  const submitBMI = (e) => {
+    e.preventDefault();
+    objUserStats = {
+      height: height,
+      weight: weight,
+      age: age,
+      gender: gender,
+    };
+    // function that calculate the BMI of the use and keep the data
+    if (answer) {
+      setAnswer(false);
+    } else {
+      setAnswer(true);
     }
 
     const submitBMR = (e) => {
@@ -84,10 +76,10 @@ const FormGoals = () => {
         } else {
             setAnswer(true)
         }
-        // function that calculate the BMI of the use and keep the data
-        //         Men: BMR = 88.362 + (13.397 x weight in kg) + (4.799 x height in cm) - (5.677 x age in years)
-        // Women: BMR = 447.593 + (9.247 x weight in kg) + (3.098 x height in cm) - (4.330 x age in years)
 
+        // function that calculate the BMI of the use and keep the data
+        // Men: BMR = 88.362 + (13.397 x weight in kg) + (4.799 x height in cm) - (5.677 x age in years)
+        // Women: BMR = 447.593 + (9.247 x weight in kg) + (3.098 x height in cm) - (4.330 x age in years)
         if (gender.toLowerCase() === "man") {
 
             let temp = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
@@ -100,14 +92,12 @@ const FormGoals = () => {
         }
     }
 
-
     const classes = useStyles();
 
     return (
         <div className={classes.container}>
-
             <div className={classes.divOne}>
-                <h2 style={{ textAlign: "center" }}>Welcome to My status page</h2>
+                <h2 style={{ textAlign: "center", textDecoration: "underline", color: "blue" }}>Welcome to My status page</h2>
                 <form className={classes.form} >
                     <TextField id="standard-secondary" label="Enter height" variant="outlined" color="secondary"
                         style={{ marginBottom: "20px" }}
@@ -137,8 +127,9 @@ const FormGoals = () => {
 
                 </form>
                 <div className={classes.button}>
-                    <button onClick={submitBMI} style={{ marginRight: "10px" }}>calculate BMI</button>
-                    <button onClick={submitBMR}>calculate BMR</button>
+                    <Button variant="contained" color="primary" onClick={submitBMI} style={{ marginRight: "10px" }} >calculate BMI</Button>
+                    <Button variant="contained" color="primary" onClick={submitBMR} style={{ marginRight: "10px" }} >calculate BMR</Button>
+                    <Button variant="contained" color="primary" onClick={submitBMR} style={{ marginRight: "10px", color: "#ea80fc" }} >Main Page</Button>
                 </div>
                 {answer && resultBMI > 0 &&
                     <div div className={classes.form}>
@@ -151,12 +142,9 @@ const FormGoals = () => {
                     </div>
                 }
             </div>
-
         </div >
 
     );
 }
 
-export default FormGoals
-
-
+export default FormGoals;
