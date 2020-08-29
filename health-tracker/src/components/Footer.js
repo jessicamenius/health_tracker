e73cd1df56526e68f4d6351f161b762e456f06f8
Steven Simon <stevierichard1@gmail.com>
2020-08-29 12:34:28 -0700

    changes the footer plus removing unwanted functions on register file

--- health-tracker/src/components/Footer.js
+++ health-tracker/src/components/Footer.js
@@ -1,53 +1,48 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
 const Footer = () => {
-
-    function Copyright() {
-        return (
-            <Typography variant="body2" color="textSecondary" align="center">
-                {"Copyright © "}
-                <Link color="inherit" href="https://material-ui.com/">
-                    Developers Jessica, Philip, Mat, Steven
-            </Link>{" "}
-                {new Date().getFullYear()}
-                {"."}
-            </Typography>
-        );
-    }
-
-    const useStyles = makeStyles((theme) => ({
-        root: {
-            display: 'flex',
-            flexDirection: 'column',
-            minHeight: "100vh",
-        },
-        main: {
-            marginTop: theme.spacing(8),
-            marginBottom: theme.spacing(2),
-        },
-        footer: {
-            padding: theme.spacing(3, 2),
-            marginTop: 'auto',
-        },
-    }));
-    const classes = useStyles();
-
+  function Copyright() {
     return (
-        <div className={classes.root}>
-            <footer footer="true" className={classes.footer}>
-                <Container maxWidth="sm">
-                    <Typography variant="body1">
-                        My Exercise Rest Nutrition Tracker (MERN-Tracker)
-                    </Typography>
-                    <Copyright />
-                </Container>
-            </footer>
-        </div >
-    );
-}
+      <Typography variant="body2" color="textSecondary" align="center">
+        {"Copyright © "}
 
-export default Footer
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    );
+  }
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: "flex",
+      flexDirection: "column",
+    },
+    main: {
+      marginTop: theme.spacing(8),
+      marginBottom: theme.spacing(2),
+    },
+    footer: {
+      padding: theme.spacing(3, 2),
+      marginTop: "auto",
+    },
+  }));
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <footer footer="true" className={classes.footer}>
+        <Container maxWidth="sm">
+          <Typography variant="body1" style={{ textAlign: "center" }}>
+            My Exercise Rest Nutrition Tracker (MERN-Tracker)
+            <Typography variant="body1" style={{ textAlign: "center" }}>
+              Developers Jessica, Philip, Mat, Steven
+            </Typography>
+          </Typography>
+          <Copyright />
+        </Container>
+      </footer>
+    </div>
+  );
+};
+export default Footer;
