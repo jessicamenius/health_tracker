339ce1e77d51172c80e260f9890b2477c5c95996
Jessica <jessicamenius@gmail.com>
2020-08-29 15:38:43 -0500

    pushing changes

--- health-tracker/src/components/Footer.js
+++ health-tracker/src/components/Footer.js
@@ -3,61 +3,10 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 const Footer = () => {
-<<<<<<< HEAD
-
-
-
-    function Copyright() {
-        return (
-            <Typography variant="body2" color="textSecondary" align="center">
-                {"Copyright © "}
-                <Link color="primary">
-                    Developers Jessica, Philip, Mat, Steven
-            </Link>{" "}
-                {new Date().getFullYear()}
-                {"."}
-            </Typography>
-        );
-    }
-
-    const useStyles = makeStyles((theme) => ({
-        root: {
-            display: 'flex',
-            flexDirection: 'column'
-        },
-        main: {
-            marginTop: theme.spacing(8),
-            marginBottom: theme.spacing(2),
-        },
-        footer: {
-            padding: theme.spacing(3, 2),
-            marginTop: 'auto',
-        },
-    }));
-    const classes = useStyles();
-
-    return (
-        <div className={classes.root}>
-            <footer footer="true" className={classes.footer}>
-                <Container maxWidth="sm">
-                    <Typography variant="body1" style={{ textAlign: "center" }}>
-                        My Exercise Rest Nutrition Tracker (MERN-Tracker)
-                    </Typography>
-                    <Copyright />
-                </Container>
-            </footer>
-        </div >
-    );
-}
-
-
-        {new Date().getFullYear()}
-=======
   function Copyright() {
     return (
       <Typography variant="body2" color="textSecondary" align="center">
         {"Copyright © "} {new Date().getFullYear()}
->>>>>>> 26c5c8f59514327147c22816480eeb20df296ae4
         {"."}
       </Typography>
     );
