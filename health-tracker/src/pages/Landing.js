49c933cf63fc0348920c9e95cceb28b5c1438291
Philip Kouchner <philipk179@gmail.com>
2020-08-31 21:21:39 -0700

    make small animation,update the landing page

--- health-tracker/src/pages/Landing.js
+++ health-tracker/src/pages/Landing.js
@@ -1,23 +1,29 @@
 import React from 'react'
 import img from './img/Logo.png'
-
+import { Fade } from 'react-animation-components'
+import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
+import Typography from '@material-ui/core/Typography';
 const Landing = () => {
 
     return (
-
         <div style={{
             display: "flex", justifyContent: "center", alignContent: "center", flexDirection: "column", width: "50%",
-            marginLeft: "25%", marginTop: "5%"
+            marginLeft: "25%", fontFamily: "cursive"
         }}>
-            <div>
-                <img src={img} style={{ height: "250px", width: "900px" }} alt="Our Compnay Logo" />
-            </div>
-            <h1 style={{ textAlign: "center" }}>Welcome to MERN</h1>
-            <h2 style={{ textAlign: "center", display: "flex", justifyContent: "center", alignContent: "center", color: "blue" }}>
-                MERN Health Tracker is a web application which utilizes the Edamam API that can be used to track daily and weekly nutritional intake,<br />
-        along with weight goals and your progress towards them. You will be able to log meals you eat and see detailed information about them,<br />
-        such as fat, protein, and carb content, along with an overall view for your intake each week.
-    </h2>
+            <Fade in enterOpacity={1.25}>
+                <Typography variant="h2" style={{ textAlign: "center", color: "#FF1493", marginBottom: "50px", marginTop: "50px", fontFamily: "cursive" }}>
+                    <DirectionsRunIcon fontSize="large" />
+                    Welcome
+                  </Typography>
+                <div>
+                    <img src={img} style={{ height: "250px", width: "900px" }} alt="Our Compnay Logo" />
+                </div>
+                <h2 style={{ textAlign: "center", display: "flex", justifyContent: "center", alignContent: "center", color: "#3F51B5" }}>
+                    MERN Health Tracker is a web application which utilizes the Edamam API that can be used to track daily and weekly nutritional intake,<br />
+                    along with weight goals and your progress towards them. You will be able to log meals you eat and see detailed information about them,<br />
+                  such as fat, protein, and carb content, along with an overall view for your intake each week.
+                 </h2>
+            </Fade>
         </div >
 
     )
