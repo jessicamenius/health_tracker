ce48498724ed21a8a3e687b4adc3aa9b5eec9806
Mat Wilmot <matwilmot90@gmail.com>
2020-09-02 21:18:32 -0700

    Working on auth/user model

--- models/user.js
+++ models/user.js
@@ -16,8 +16,8 @@ module.exports = (sequelize, DataTypes) => {
       },
     },
     password: { type: DataTypes.STRING, allowNull: false },
-    firstName: { type: DataTypes.STRING, allowNull: false },
-    lastName: { type: DataTypes.STRING, allowNull: false },
+    // firstName: { type: DataTypes.STRING, allowNull: false },
+    // lastName: { type: DataTypes.STRING, allowNull: false },
   });
 
   User.prototype.validPassword = function (password) {
