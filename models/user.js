e4bc6c312c52c598e393b2b55f377b187aed51be
Jessica <jessicamenius@gmail.com>
2020-09-08 15:09:10 -0500

    pushing updates

--- models/user.js
+++ models/user.js
@@ -15,11 +15,7 @@ module.exports = function (sequelize, DataTypes) {
         isEmail: true,
       },
     },
-<<<<<<< HEAD
-    userName: {
-=======
     password: {
->>>>>>> 6dbbfc55cf727e685ddea0238b82205e9a4885bf
       type: DataTypes.STRING,
       allowNull: false,
     },
@@ -49,10 +45,7 @@ module.exports = function (sequelize, DataTypes) {
     User.hasOne(models.Stats, {
       onDelete: "cascade",
     });
-<<<<<<< HEAD
 
-=======
->>>>>>> 6dbbfc55cf727e685ddea0238b82205e9a4885bf
     User.hasMany(models.FoodLog, {
       onDelete: "cascade",
     });
