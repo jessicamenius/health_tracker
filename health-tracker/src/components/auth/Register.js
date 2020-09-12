5a2b0f96bda6bb27bb6dce672c09f628a846806c
Steven Simon <stevierichard1@gmail.com>
2020-09-12 10:18:37 -0700

    fixes nav, and footer bugs

--- health-tracker/src/components/auth/Register.js
+++ health-tracker/src/components/auth/Register.js
@@ -7,8 +7,6 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
@@ -180,18 +178,18 @@ export default function Register() {
                 autoComplete="current-password"
                 onChange={(e) => setPasswordCheck(e.target.value)}
               />
+              <Link href="/userGoals" variant="body2">
+                <Button
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                  onClick={submit}
+                >
+                  REGISTER
+                </Button>
+              </Link>
             </Grid>
-            <Link href="/userGoals" variant="body2">
-              <Button
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}
-                onClick={submit}
-              >
-                REGISTER
-              </Button>
-            </Link>
 
             <Grid container justify="flex-end">
               <Grid item>
