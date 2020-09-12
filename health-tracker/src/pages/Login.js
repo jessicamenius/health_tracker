e53c47df90e70d574c21b4178d4fe8de612d2978
jessicamenius <jessicamenius@gmail.com>
2020-09-12 15:30:37 -0500

    fixing login page function

--- health-tracker/src/pages/Login.js
+++ health-tracker/src/pages/Login.js
@@ -1,10 +1,12 @@
 import React from "react";
 import LoginCom from "../components/auth/LoginCom";
 
-export default Login = (props) => {
+const Login = (props) => {
   return (
     <div>
       <LoginCom userData={props.userData} setUserData={props.setUserData} />
     </div>
   );
 };
+
+export default Login;
