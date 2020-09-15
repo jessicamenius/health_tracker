601899a16b052630ca231058d8880de87d940078
jessicamenius <jessicamenius@gmail.com>
2020-09-14 21:26:26 -0500

    committing before pulling

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const session = require("express-session");
 
 const db = require("./models");
 const PORT = process.env.PORT || 5000;
@@ -18,14 +17,6 @@ require("dotenv").config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
-// app.use(
-//   session({
-//     secret: process.env.SECRET,
-//     resave: true,
-//     saveUninitialized: true,
-//   })
-// );
 
 app.use(passport.initialize());
 app.use(passport.session());
