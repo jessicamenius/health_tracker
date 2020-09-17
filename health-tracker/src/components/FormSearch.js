b87af7dbb404e7d7dbe1235cdfb377a2886ddff6
Steven Simon <stevierichard1@gmail.com>
2020-09-16 18:56:00 -0700

    polish dashboard & add readme

--- health-tracker/src/components/FormSearch.js
+++ health-tracker/src/components/FormSearch.js
@@ -1,44 +1,40 @@
-import React, { useState } from 'react'
-import TextField from '@material-ui/core/TextField';
-import FormControl from '@material-ui/core/FormControl';
-import MenuItem from '@material-ui/core/MenuItem';
-import { Button, Select } from '@material-ui/core';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import API from '../utils/API'
-import AlertMessage from '../components/AlertMessage'
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
+import MenuItem from "@material-ui/core/MenuItem";
+import { Button, Select } from "@material-ui/core";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import API from "../utils/API";
+import AlertMessage from "../components/AlertMessage";
 const FormSearch = (props) => {
-
-  const [measure, setMeasure] = useState('Pounds');
+  const [measure, setMeasure] = useState("Pounds");
   const [amount, setAmount] = useState("");
   const [searchFood, setSearchFood] = useState("");
   const [arrayFood, setArrayFood] = useState([]);
   const [flag, setFlag] = useState(false);
   const [status, setStatusBase] = useState("");
 
-
   const numInput = (e) => {
     setAmount(e.target.value);
-  }
+  };
   const measureInput = (e) => {
     setMeasure(e.target.value);
-  }
+  };
 
   const onKeyPress = (e) => {
     setSearchFood(e.target.value);
     const text = e.target.value;
     if (text.length > 1) {
-      API.autocomplete(text).then(res => {
+      API.autocomplete(text).then((res) => {
         let arrayInput = [];
-        console.log(res.data)
+        console.log(res.data);
         arrayInput = [...res.data];
         setArrayFood([...arrayInput]);
       });
     }
-
-  }
+  };
   const onSave = (event, newValue) => {
     setSearchFood(newValue);
-
   };
   const submit = (e) => {
     e.preventDefault();
@@ -51,27 +47,28 @@ const FormSearch = (props) => {
       setFlag(true);
       setStatusBase({ msg: "Error - Invalid Input", key: Math.random() });
     }
+  };
 
-  }
-
-  const arr = [<MenuItem value="kilogram"> Kilogram</MenuItem>,
-  <MenuItem value={"pound"}>Pound</MenuItem>,
-  <MenuItem value={"ounce"}>Ounce</MenuItem>,
-  <MenuItem value={"gram"}>Gram</MenuItem>,
-  <MenuItem value={"pinch"}>Pinch</MenuItem>,
-  <MenuItem value={"liter"}>Liter</MenuItem>,
-  <MenuItem value={"fluid_ounce"}>fluid-Ounce</MenuItem>,
-  <MenuItem value={"pint"}>Pint</MenuItem>,
-  <MenuItem value={"quart"}>Quart</MenuItem>,
-  <MenuItem value={"milliliter"}>Milliliter</MenuItem>,
-  <MenuItem value={"drop"}>Drop</MenuItem>,
-  <MenuItem value={"cup"}>Cup</MenuItem>,
-  <MenuItem value={"tablespoon"}>Tablespoon</MenuItem>,
-  <MenuItem value={"teaspoon"}>Teaspoon</MenuItem>];
+  const arr = [
+    <MenuItem value="kilogram"> Kilogram</MenuItem>,
+    <MenuItem value={"pound"}>Pound</MenuItem>,
+    <MenuItem value={"ounce"}>Ounce</MenuItem>,
+    <MenuItem value={"gram"}>Gram</MenuItem>,
+    <MenuItem value={"pinch"}>Pinch</MenuItem>,
+    <MenuItem value={"liter"}>Liter</MenuItem>,
+    <MenuItem value={"fluid_ounce"}>fluid-Ounce</MenuItem>,
+    <MenuItem value={"pint"}>Pint</MenuItem>,
+    <MenuItem value={"quart"}>Quart</MenuItem>,
+    <MenuItem value={"milliliter"}>Milliliter</MenuItem>,
+    <MenuItem value={"drop"}>Drop</MenuItem>,
+    <MenuItem value={"cup"}>Cup</MenuItem>,
+    <MenuItem value={"tablespoon"}>Tablespoon</MenuItem>,
+    <MenuItem value={"teaspoon"}>Teaspoon</MenuItem>,
+  ];
 
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
-      <FormControl variant="outlined" style={{ marginRight: "50px" }} >
+      <FormControl variant="outlined" style={{ marginRight: "50px" }}>
         <Autocomplete
           options={arrayFood}
           id="controlled-demo"
@@ -85,6 +82,7 @@ const FormSearch = (props) => {
         <TextField
           id="outlined-number"
           variant="outlined"
+          label="Enter metric number in form of kg or g or pound or ounce "
           value={amount}
           className="mb-4"
           onChange={numInput}
@@ -93,32 +91,17 @@ const FormSearch = (props) => {
         <Select
           value={measure}
           onChange={measureInput}
-          style={{ marginBottom: "25px" }} >
+          style={{ marginBottom: "25px" }}
+        >
           {arr}
         </Select>
-        <Button onClick={submit} variant="contained" color="primary">Submit
-            {flag ? (
-            <AlertMessage key={status.key} message={status.msg} />
-          ) : null}
+        <Button onClick={submit} variant="contained" color="primary">
+          Submit
+          {flag ? <AlertMessage key={status.key} message={status.msg} /> : null}
         </Button>
       </FormControl>
-
-
     </div>
+  );
+};
 
-  )
-}
-
-
-export default FormSearch
-
-
-
-
-
-
-
-
-
-
-
+export default FormSearch;
