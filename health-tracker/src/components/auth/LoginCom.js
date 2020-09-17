37136911620f42badeaf19935afdd39d86e28a8f
Philip Kouchner <philipk179@gmail.com>
2020-09-16 20:17:57 -0700

    delete console logs

--- health-tracker/src/components/auth/LoginCom.js
+++ health-tracker/src/components/auth/LoginCom.js
@@ -7,8 +7,6 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
@@ -68,7 +66,7 @@ export default function LoginCom(props) {
       backgroundColor: theme.palette.secondary.main,
     },
     form: {
-      width: "100%", // Fix IE 11 issue.
+      width: "100%",
       marginTop: theme.spacing(1),
     },
     submit: {
